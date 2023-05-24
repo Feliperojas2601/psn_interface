a085e7b9c50a56d33ca7c103f1d8531455196643
Felipe Rojas <jrojasce@unal.edu.co>
2023-05-24 17:53:24 -0500

    initial structure

---
+++ src/main/java/com/example/psn_interface/PsnInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.psn_interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsnInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsnInterfaceApplication.class, args);
+	}
+
+}
