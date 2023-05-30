ae2a89c8f266cc5c2efcc223f48cae353f8e57da
Felipe Rojas <jrojasce@unal.edu.co>
2023-05-29 20:48:43 -0500

    error init spring

--- src/main/java/com/example/psn_interface/PsnInterfaceApplication.java
+++ src/main/java/com/example/psn_interface/PsnInterfaceApplication.java
@@ -2,8 +2,10 @@ package com.example.psn_interface;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.psn_interface"})
 public class PsnInterfaceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/psn_interface/entities/TwoF.java
@@ -0,0 +1,10 @@
+package com.example.psn_interface.entities;
+
+public class TwoF {
+
+  public String name;
+
+  public TwoF(String name) {
+      this.name = name;
+  }
+}
