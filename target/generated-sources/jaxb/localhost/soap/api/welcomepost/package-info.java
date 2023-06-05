96f29aa606d660390e436587c90ab93ba05da3c2
dbustos106 <dbustos@unal.edu.co>
2023-06-05 00:06:42 -0500

    jar funcionando uwu

---
+++ target/generated-sources/jaxb/localhost/soap/api/welcomepost/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2023.06.04 a las 11:47:21 PM COT 
+//
+
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://localhost/soap/api/WelcomePost", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package localhost.soap.api.welcomepost;
